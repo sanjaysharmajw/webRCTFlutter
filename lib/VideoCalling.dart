1fdc42bbf6666104ce9d8ef1a1d8b15e68449a14
Sanjay Sharma <58361466+sanjaysharmajw@users.noreply.github.com>
2025-02-21 17:09:04 +0530

    Video call socket

---
+++ lib/VideoCalling.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:flutter/material.dart';
+
+class VideoCalling extends StatefulWidget {
+  const VideoCalling({super.key});
+
+  @override
+  State<VideoCalling> createState() => _VideoCallingState();
+}
+
+class _VideoCallingState extends State<VideoCalling> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
